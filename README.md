029099a7c9c9636a9a39958c96078a8606d64f23
Ks_code <kamsuleader@gmail.com>
2025-05-30 00:26:08 +0100

    Remplacement complet par la version finale

--- README.md
+++
@@ -1,2 +0,0 @@
-# projet_tuteure
-Projet portant sur une plateforme d'information agricole et de mise en relation
--- views/blog.php
+++
@@ -1,5 +0,0 @@
-
-<main>
-    <h2>blog</h2>
-    <p>Bienvenue sur la page blog. Ici, vous pouvez en savoir plus sur notre site.</p>
-</main>
