1ee260d6976261e009e08b0f50522a124807faca
Douanla <juniordouanlafopa@gmail.com>
2025-05-30 11:57:29 +0100

    Ajout de mon document à la branche interface

---
+++ README.md
@@ -0,0 +1,2 @@
+# projet_tuteure
+Projet portant sur une plateforme d'information agricole et de mise en relation
---
+++ views/footer.php
@@ -0,0 +1,36 @@
+<footer id="footer" class="footer dark-background ">
+    <div class="copyright text-center">
+      <div class="container d-flex flex-column flex-lg-row justify-content-center justify-content-lg-between align-items-center">
+
+        <div class="d-flex flex-column align-items-center align-items-lg-start">
+          <div>
+            © Copyright <strong><span>2025 plateforme d'information agricole</span></strong>.Tous droits réservés.
+          </div>
+          <div class="credits">
+          </div>
+        </div>
+
+        <div class="social-links order-first order-lg-last mb-3 mb-lg-0">
+          <a href=""><i class="bi bi-twitter-x"></i></a>
+          <a href=""><i class="bi bi-facebook"></i></a>
+          <a href=""><i class="bi bi-instagram"></i></a>
+          <a href=""><i class="bi bi-linkedin"></i></a>
+        </div>
+
+      </div>
+    </div>
+  </footer>
+
+  <!-- Scroll Top -->
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- vendors JS Files -->
+  <script src="/mvc/assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/mvc/assets/vendors/aos/aos.js"></script>
+
+  <!-- Main JS File -->
+  <script src="/mvc/assets/js/main.js"></script>
+
+</body>
+
+</html>
